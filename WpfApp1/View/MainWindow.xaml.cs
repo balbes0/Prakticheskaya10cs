aa4f58964d7d6bcfdc8a3d41c65bb00619c8b229
balbes0 <balbes0@DESKTOP-1QE07C8>
2024-06-15 22:58:18 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/View/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using WpfApp1.ViewModel;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+        #region TitleBar
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    var mousePos = PointToScreen(e.GetPosition(this));
+                    WindowState = WindowState.Normal;
+                    Left = mousePos.X - (ActualWidth / 2);
+                    Top = mousePos.Y - (30 / 2);
+                }
+                DragMove();
+            }
+        }
+        #endregion
+    }
+}
---
+++ WpfApp1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows.Media.Imaging;
+using System.Windows;
+using WpfApp1.ViewModel.Helpers;
+
+namespace WpfApp1.ViewModel
+{
+    class MainWindowViewModel : BindingHelper
+    {
+        private bool isDarkTheme = false;
+        public MainWindowViewModel()
+        {
+            SwitchThemeCommand = new BindableCommand(_ => SwitchTheme());
+            directions = new List<string>
+            {
+                "Офтальмолог",
+                "Кардиолог",
+                "Невролог"
+            };
+        }
+
+        #region Свойства
+        private DateTime dateNow = DateTime.Now;
+        public DateTime DateNow
+        {
+            get { return dateNow; }
+            set
+            {
+                dateNow = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private BitmapImage imageTheme = new BitmapImage(new Uri("../Images/NightTheme.png", UriKind.Relative));
+        public BitmapImage ImageTheme
+        {
+            get { return imageTheme; }
+            set
+            {
+                imageTheme = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<string> directions;
+        public List<string> Directions
+        {
+            get { return directions; }
+            set
+            {
+                directions = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Команды
+        public BindableCommand SwitchThemeCommand { get; set; }
+        #endregion
+
+        private void SwitchTheme()
+        {
+            // Применяем противоположную тему
+            if (isDarkTheme)
+            {
+                ApplyLightTheme(); // Переключаем на светлую тему
+                isDarkTheme = false;
+            }
+            else
+            {
+                ApplyDarkTheme(); // Переключаем на тёмную тему
+                isDarkTheme = true;
+            }
+        }
+
+        // Для переключения на темную тему
+        private void ApplyDarkTheme()
+        {
+            Application.Current.Resources.MergedDictionaries[2] = new ResourceDictionary()
+            {
+                Source = new Uri("pack://application:,,,/ThemeLibrary;component/Themes/DarkTheme.xaml")
+            };
+            ImageTheme = new BitmapImage(new Uri("../Images/LightTheme.png", UriKind.Relative));
+        }
+
+        // Для переключения на светлую тему
+        private void ApplyLightTheme()
+        {
+            Application.Current.Resources.MergedDictionaries[2] = new ResourceDictionary()
+            {
+                Source = new Uri("pack://application:,,,/ThemeLibrary;component/Themes/LightTheme.xaml")
+            };
+            ImageTheme = new BitmapImage(new Uri("../Images/NightTheme.png", UriKind.Relative));
+        }
+    }
+}
